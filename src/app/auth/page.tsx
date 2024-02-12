0ed0c3348b2ff192bbd37088746a779562fc534a
Gavin <86824319+GavinITP@users.noreply.github.com>
2024-02-12 19:09:04 +0700

    create sign up ui

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,13 +3,21 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Login = () => {
   return (
     <main className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
       <Tabs
         defaultValue="login"
-        className=" w-[630px] rounded-md border bg-white shadow-lg"
+        className=" w-[630px] rounded-md border bg-white px-12 py-6 shadow-lg"
       >
         <p className="my-6 text-center text-3xl font-bold italic text-indigo-700">
           DebTour
@@ -20,6 +28,7 @@ const Login = () => {
           <TabsTrigger value="signup">Sign up</TabsTrigger>
         </TabsList>
 
+        {/* log in */}
         <TabsContent value="login">
           <div className="mx-auto my-6 grid w-full max-w-md items-center gap-6">
             <div className="flex flex-col gap-2">
@@ -44,7 +53,33 @@ const Login = () => {
             </button>
           </div>
         </TabsContent>
-        <TabsContent value="signup">Change your password here.</TabsContent>
+
+        {/* sign up */}
+        <TabsContent value="signup">
+          <div className="mx-auto w-full max-w-lg pb-10">
+            <h2 className=" mb-6 mt-10 text-center text-lg font-bold">
+              Register As
+            </h2>
+            <div className="flex gap-4">
+              <Card className="duration-75 hover:scale-105 hover:cursor-pointer hover:bg-gray-200">
+                <CardHeader>
+                  <CardTitle className="text-xl">Tourist</CardTitle>
+                  <CardDescription>
+                    Find interesting tours and have fun!
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+              <Card className="duration-75 hover:scale-105 hover:cursor-pointer hover:bg-gray-200">
+                <CardHeader>
+                  <CardTitle className="text-xl">Agency</CardTitle>
+                  <CardDescription>
+                    Post your tours and gain the profit!
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </div>
+          </div>
+        </TabsContent>
       </Tabs>
     </main>
   );
