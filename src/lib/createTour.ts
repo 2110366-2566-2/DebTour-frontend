c41103d4efb74e94b1d9e6f656b369fea0fb8494
Tawatwee Aiemarreerat <punnyoz1103@gmail.com>
2024-02-12 13:45:29 +0700

    fix some names in form and add api call

--- src/lib/createTour.ts
+++ src/lib/createTour.ts
@@ -1,3 +1,15 @@
-export default async function createTour(token: string, tour: any) {
-        
+export default async function createTour(token: string, tour: object) {
+    const response = await fetch("http://13.50.91.47:9000/api/v1/tours", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            // "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify(tour),
+    });
+    console.log(response);
+    if (!response.ok) {
+        throw new Error("Failed to create tour");
+    }
+    return response.json();
 }
