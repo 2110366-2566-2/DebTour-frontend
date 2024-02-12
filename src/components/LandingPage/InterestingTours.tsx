e0d2a9724c974891352941844d0431d102be0f70
kyrov <pho445.2013@gmail.com>
2024-02-12 17:25:32 +0700

    Making landing_page responsive

--- src/components/LandingPage/InterestingTours.tsx
+++ src/components/LandingPage/InterestingTours.tsx
@@ -4,7 +4,9 @@ const InterestingTours = () => {
   return (
     <div className="bg-[#E0E7FF]">
       <p className="m-5">
-        <span className="text-3xl font-bold text-black">Interesting Tours</span>
+        <span className="text-xl font-bold text-black md:text-3xl">
+          Interesting Tours
+        </span>
       </p>
     </div>
   );
--- src/components/LandingPage/LandingBanner.tsx
+++ src/components/LandingPage/LandingBanner.tsx
@@ -3,30 +3,38 @@ import Image from "next/image";
 
 const LandingBanner = () => {
   return (
-    <div className="flex w-full flex-row place-items-center justify-center gap-5 bg-[#E0E7FF]">
+    <div className="flex w-full flex-row place-items-center justify-center gap-5 bg-[#E0E7FF] px-5 py-5 md:px-6 md:py-10">
       {/* Left */}
-      <div className="m-5 flex justify-center">
+      <div className="m-1 flex justify-center md:m-5">
         <p>
-          <span className="text-7xl font-bold text-black">Explore </span>
-          <span className="text-7xl font-bold text-[#6366F1]">100+</span>
+          <span className="text-3xl font-bold text-black md:text-7xl">
+            Explore{" "}
+          </span>
+          <span className="text-3xl font-bold text-[#6366F1] md:text-7xl">
+            100+
+          </span>
           <br />
-          <span className="text-7xl font-bold text-[#6366F1]">Tour </span>
-          <span className="text-sm font-bold text-black md:text-7xl">in </span>
-          <span className="text-7xl font-bold text-[#6366F1]">DebTour</span>
+          <span className="text-3xl font-bold text-[#6366F1] md:text-7xl">
+            Tour{" "}
+          </span>
+          <span className="text-3xl font-bold text-black md:text-7xl">in </span>
+          <span className="text-3xl font-bold text-[#6366F1] md:text-7xl">
+            DebTour
+          </span>
           <br />
           <br />
-          <span className="text-xl text-black">
+          <span className="text-sm text-black md:text-xl">
             The platform for tourist that finding high quality tour
           </span>
         </p>
       </div>
       {/* Right */}
-      <div className="m-5">
+      <div className="m-1 flex w-3/5 justify-center md:m-5 md:w-2/4">
         <Image
           src="/LandingPage-Minimap.png"
           alt="landing"
-          width={500}
-          height={500}
+          width={600}
+          height={600}
         />
       </div>
     </div>
