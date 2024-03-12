bff5e552c814560d4853b2ce566aaf4146385b48
Tawatwee Aiemarreerat <punnyoz1103@gmail.com>
2024-03-13 00:33:38 +0700

    refactor createReview

--- src/lib/createReview.ts
+++ src/lib/createReview.ts
@@ -1,7 +1,7 @@
 import reviewFormSchema from "@/model/reviewFormSchema"
 import { z } from "zod"
 
-export default async function createReview(reviewData: z.infer<typeof reviewFormSchema>, token: string) {
+export default async function createReview(token: string, reviewData: z.infer<typeof reviewFormSchema>) {
     const response = await fetch(`${process.env.BACKEND_URL}/api/v1/reviews/tour/${reviewData.tourId}`, {
         method: "POST",
         headers: {
