9a956f0d04a063aff6fc424dc213e614932ace9a
Thammarit Phiwton <poomz180@gmail.com>
2024-03-27 22:08:28 +0700

    fix fetch suggestionTable tourist

--- src/lib/getAllSuggestions.ts
+++ src/lib/getAllSuggestions.ts
@@ -1,19 +1,35 @@
 
-export default async function getAllSuggestions(token: string| undefined) {
-    if (!token) {
+export default async function getAllSuggestions(username:string|undefined, role:string|undefined, token: string| undefined) {
+    if (!username || !token) {
         return
     }
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/suggestions/location`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
-        },
-    });
+    if (role === "Tourist") {
+        const response = await fetch(`${process.env.BACKEND_URL}/api/v1/suggestions/tourist/${username}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+            },
+        });
 
-    if (!response.ok) {
-        throw new Error("Failed to get suggestions");
+        if (!response.ok) {
+            throw new Error("Failed to get suggestions");
+        }
+
+        return response.json();
+    } else {
+        const response = await fetch(`${process.env.BACKEND_URL}/api/v1/suggestions/location`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to get suggestions");
+        }
+
+        return response.json();
     }
-
-    return response.json();
 }
