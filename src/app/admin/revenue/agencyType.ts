82f99ae909b0ad796bce2dd80476c27f5a46a5bd
GavinITP <86824319+GavinITP@users.noreply.github.com>
2024-04-19 21:26:26 +0700

    refactor: extract component

---
+++ src/app/admin/revenue/agencyType.ts
@@ -0,0 +1,13 @@
+export interface AgencyType {
+  agencyName: string;
+  approveTime: string;
+  authorizeAdminUsername: string;
+  authorizeStatus: string;
+  bankAccount: string;
+  bankName: string;
+  email: string;
+  image: string;
+  licenseNo: string;
+  phone: string;
+  username: string;
+}
