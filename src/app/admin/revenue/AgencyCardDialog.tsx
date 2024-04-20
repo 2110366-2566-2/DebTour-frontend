9ca7d6156f5e4f4560f0e77ebdc00fd5d9f82cf9
GavinITP <86824319+GavinITP@users.noreply.github.com>
2024-04-20 21:54:08 +0700

    fix: add missing data

--- src/app/admin/revenue/AgencyCardDialog.tsx
+++ src/app/admin/revenue/AgencyCardDialog.tsx
@@ -13,6 +13,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Loader2 } from "lucide-react";
 import { useSession } from "next-auth/react";
+import { format, parseISO } from "date-fns";
 
 const AgencyCardDialog = ({ username }: { username: string }) => {
   const { data: session } = useSession();
@@ -108,6 +109,17 @@ const AgencyCardDialog = ({ username }: { username: string }) => {
     );
   }
 
+  const {
+    amount,
+    method,
+    status,
+    stripeID,
+    timestamp,
+    tourId,
+    touristUsername,
+    transactionId,
+  } = agencyRevenue;
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -125,51 +137,67 @@ const AgencyCardDialog = ({ username }: { username: string }) => {
             <Label htmlFor="amount" className="text-right">
               Amount
             </Label>
-            <p id="amount" className="col-span-3"></p>
+            <p id="amount" className="col-span-3">
+              {amount || "-"}
+            </p>
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="method" className="text-right">
               Payment Method
             </Label>
-            <p id="method" className="col-span-3"></p>
+            <p id="method" className="col-span-3">
+              {method || "-"}
+            </p>
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="status" className="text-right">
               Status
             </Label>
             <p id="status" className="col-span-3">
-              {status}
+              {status || "-"}
             </p>
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="stripeID" className="text-right">
               Stripe ID
             </Label>
-            <p id="stripeID" className="col-span-3"></p>
+            <p id="stripeID" className="col-span-3">
+              {stripeID || "-"}
+            </p>
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="timestamp" className="text-right">
               Timestamp
             </Label>
-            <p id="timestamp" className="col-span-3"></p>
+            <p id="timestamp" className="col-span-3">
+              {timestamp
+                ? format(parseISO(timestamp), "yyyy-MM-dd HH:mm:ss")
+                : "-"}
+            </p>
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="tourId" className="text-right">
               Tour ID
             </Label>
-            <p id="tourId" className="col-span-3"></p>
+            <p id="tourId" className="col-span-3">
+              {tourId || "-"}
+            </p>
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="touristUsername" className="text-right">
               Tourist Username
             </Label>
-            <p id="touristUsername" className="col-span-3"></p>
+            <p id="touristUsername" className="col-span-3">
+              {touristUsername || "-"}
+            </p>
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="transactionId" className="text-right">
               Transaction ID
             </Label>
-            <p id="transactionId" className="col-span-3"></p>
+            <p id="transactionId" className="col-span-3">
+              {transactionId || "-"}
+            </p>
           </div>
         </div>
         <DialogFooter></DialogFooter>
