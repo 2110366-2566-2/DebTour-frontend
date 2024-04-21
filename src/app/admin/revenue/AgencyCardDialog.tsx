61fe3901258c50eb0dd13dffdcda449c5088d7e8
GavinITP <86824319+GavinITP@users.noreply.github.com>
2024-04-21 11:27:51 +0700

    feat: TransactionHistoryCard

--- src/app/admin/revenue/AgencyCardDialog.tsx
+++ src/app/admin/revenue/AgencyCardDialog.tsx
@@ -8,17 +8,17 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Loader2 } from "lucide-react";
 import { useSession } from "next-auth/react";
-import { format, parseISO } from "date-fns";
+import TransactionHistoryCard from "./TransactionHistoryCard";
+import { Key } from "react";
 
 const AgencyCardDialog = ({ username }: { username: string }) => {
   const { data: session } = useSession();
 
-  async function getAgencyRevenue() {
+  async function getAgenciesRevenue() {
     const token = session?.user?.serverToken;
     const backendUrl = process.env.BACKEND_URL;
 
@@ -35,15 +35,13 @@ const AgencyCardDialog = ({ username }: { username: string }) => {
   }
 
   const {
-    data: agencyRevenue,
+    data: agenciesRevenue,
     isLoading,
     error,
   } = useQuery({
-    queryFn: () => getAgencyRevenue(),
-    queryKey: ["agencyRevenue"],
+    queryFn: () => getAgenciesRevenue(),
+    queryKey: ["agenciesRevenue"],
   });
-
-  // console.log("agencyRevenue", agencyRevenue);
 
   if (isLoading) {
     return (
@@ -87,39 +85,6 @@ const AgencyCardDialog = ({ username }: { username: string }) => {
     );
   }
 
-  if (!agencyRevenue) {
-    return (
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline">Details</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Agency Revenue Details</DialogTitle>
-            <DialogDescription>
-              {`A concise information of an agency's income sources`}
-            </DialogDescription>
-          </DialogHeader>
-          <div className="mx-auto my-auto flex justify-center text-center">
-            This agency has no revenue information.
-          </div>
-          <DialogFooter></DialogFooter>
-        </DialogContent>
-      </Dialog>
-    );
-  }
-
-  const {
-    amount,
-    method,
-    status,
-    stripeID,
-    timestamp,
-    tourId,
-    touristUsername,
-    transactionId,
-  } = agencyRevenue;
-
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -132,74 +97,14 @@ const AgencyCardDialog = ({ username }: { username: string }) => {
             {`A concise information of an agency's income sources`}
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="amount" className="text-right">
-              Amount
-            </Label>
-            <p id="amount" className="col-span-3">
-              {amount || "-"}
-            </p>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="method" className="text-right">
-              Payment Method
-            </Label>
-            <p id="method" className="col-span-3">
-              {method || "-"}
-            </p>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="status" className="text-right">
-              Status
-            </Label>
-            <p id="status" className="col-span-3">
-              {status || "-"}
-            </p>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="stripeID" className="text-right">
-              Stripe ID
-            </Label>
-            <p id="stripeID" className="col-span-3">
-              {stripeID || "-"}
-            </p>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="timestamp" className="text-right">
-              Timestamp
-            </Label>
-            <p id="timestamp" className="col-span-3">
-              {timestamp
-                ? format(parseISO(timestamp), "yyyy-MM-dd HH:mm:ss")
-                : "-"}
-            </p>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="tourId" className="text-right">
-              Tour ID
-            </Label>
-            <p id="tourId" className="col-span-3">
-              {tourId || "-"}
-            </p>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="touristUsername" className="text-right">
-              Tourist Username
-            </Label>
-            <p id="touristUsername" className="col-span-3">
-              {touristUsername || "-"}
-            </p>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="transactionId" className="text-right">
-              Transaction ID
-            </Label>
-            <p id="transactionId" className="col-span-3">
-              {transactionId || "-"}
-            </p>
-          </div>
-        </div>
+        {agenciesRevenue ? (
+          agenciesRevenue.map((a: any, index: Key) => (
+            <TransactionHistoryCard key={index} agencyRevenue={a} />
+          ))
+        ) : (
+          <p>This agency has no revenue history.</p>
+        )}
+
         <DialogFooter></DialogFooter>
       </DialogContent>
     </Dialog>
