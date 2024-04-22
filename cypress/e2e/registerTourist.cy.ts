247e42cd42447cc3e0df37e5615b189132c34d50
Thammarit Phiwton <poomz180@gmail.com>
2024-04-22 21:20:45 +0700

    add test registerTourist

--- cypress/e2e/registerTourist.cy.ts
+++ cypress/e2e/registerTourist.cy.ts
@@ -1,9 +1,43 @@
-describe('test register tourist', () => {
-    it('valid input ', () => {
-        cy.visit('https://deb-tour.vercel.app/auth/signup/tourist')
-        cy.get('input[name="phone"]').type("0901234567")
-        cy.get('input[name="citizenId"]').type("1234567890123")
-        cy.get('input[name="firstname"]').type('Tawatwee')
-        cy.get('input[name="lastname"]').type('Aiemareerat')
+describe('Register Tourist', () => {
+    beforeEach(() => {
+    })
+    it('Register Tourist ', () => {
+        cy.fixture('registerTourist.json').then((tourist) => {
+            for(let i in tourist) {
+                cy.visit('/auth/signup/tourist')
+
+                cy.get('input[name="phone"]').type(tourist[i].phone)
+                cy.wait(500)
+                cy.get('input[name="citizenId"]').type(tourist[i].citizenID)
+                cy.wait(500)
+                cy.get('input[name="firstname"]').type(tourist[i].firstname)
+                cy.wait(500)
+                cy.get('input[name="lastname"]').type(tourist[i].lastname)
+                cy.wait(500)
+                cy.get('button').contains('Next').click().click()
+                cy.wait(500)
+
+                cy.get('textarea[name="address"]').clear()
+                cy.get('textarea[name="address"]').type(tourist[i].address)
+                cy.wait(500)
+
+                if (tourist[i].expected == 'valid') {
+                    cy.get("p[id^=':r'][id$=':-form-item-message']").should("not.exist");
+                    cy.get('button').contains('Previous').click().click()
+                    cy.wait(500)
+                    cy.get("p[id^=':r'][id$=':-form-item-address']").should("not.exist");
+                } else {
+                    cy.get('body').then((body) => {
+                        if (body.find("p[id^=':r'][id$=':-form-item-message']").length > 0) {
+                            cy.get("p[id^=':r'][id$=':-form-item-message']").should("exist");
+                        }
+                    })
+                    cy.wait(500)
+                    cy.get('button').contains('Previous').click().click()
+                    cy.get('p').contains('must').should('exist')
+                }
+                cy.wait(3000)
+            }
+        })
     })
 })
